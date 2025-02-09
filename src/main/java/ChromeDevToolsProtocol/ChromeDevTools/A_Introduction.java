1efb52c7c00c9d0491435cea8d4aaf1697142672
aelaina100 <aelaina100@gmail.com>
2025-02-09 02:11:11 -0500

    Under Construction-Composing the [ReadMe] file: What is chromeDev Tools. 1st commit ever.Feb9th,2025

---
+++ src/main/java/ChromeDevToolsProtocol/ChromeDevTools/A_Introduction.java
@@ -0,0 +1,13 @@
+package ChromeDevToolsProtocol.ChromeDevTools;
+
+public class A_Introduction {
+	
+	/* Start by reading the [READ ME] file situated on the project level.
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
+}
---
+++ src/main/java/ChromeDevToolsProtocol/ChromeDevTools/B_EmulateMobileOnWebpage.java
@@ -0,0 +1,13 @@
+package ChromeDevToolsProtocol.ChromeDevTools;
+
+import org.testng.annotations.Test;
+
+public class B_EmulateMobileOnWebpage {
+	
+	@Test
+	public void emulateMobileOnWeb()
+	{
+		
+	}
+
+}
