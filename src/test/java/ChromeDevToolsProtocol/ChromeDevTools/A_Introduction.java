d1658b3900848b1ad4632f814dea0f3c2cc85f0e
aelaina100 <aelaina100@gmail.com>
2025-02-09 16:42:12 -0500

    Adding notes to [ReadMe-ChromeDevTool] text file. Commit of Feb9th,2025

---
+++ src/test/java/ChromeDevToolsProtocol/ChromeDevTools/A_Introduction.java
@@ -0,0 +1,13 @@
+package ChromeDevToolsProtocol.ChromeDevTools;
+
+public class A_Introduction {
+	
+	/* Start by reading the [ReadMe-ChromeDevTools] file situated on the project level.
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
+}
